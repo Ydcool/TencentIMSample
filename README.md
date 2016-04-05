376da59369fe9670c497083db6a9a118ae05833b
Donny <hi@ydcool.me>
2016-04-05 12:43:32 +0800

    update sdk version to 1.8

--- README.md
+++ README.md
@@ -4,5 +4,83 @@
 
 源代码来自 [官网](http://www.qcloud.com/product/im.html)，本项目是官网的 Android Studio 移植版本。( 官网 Eclipse 版本槽点太多! 😒 )  
 
-
 可直接运行使用的 [Demo Apk 下载](https://github.com/Ydcool/TencentIMSample/raw/master/demo/demo-release-unsigned.apk)
+
+-----------------------------------------------------------------------------------------------------------
+#### 使用说明
+
+1 . 由于离线推送通知依赖于特定的service，所以集成的时候必须在 `AndroidManifest.xml` 的 `<application> </application>` 中添加如下配置：
+
+```xml
+          <!-- 【必须】消息收发service -->
+         <service
+            android:name="com.tencent.qalsdk.service.QalService"
+            android:exported="true"
+            android:persistent="true"
+            android:process=":QALSERVICE" >
+        </service>  
+		
+        <!-- 【必须】 离线消息广播接收器 -->
+         <receiver
+            android:name="com.tencent.qalsdk.QALBroadcastReceiver">
+            <intent-filter>
+                <action android:name="com.tencent.qalsdk.broadcast.qal" />
+            </intent-filter>
+        </receiver>
+        
+        <!-- 【必须】 qal service广播接收,自启动 -->
+        <receiver 
+             android:name="com.tencent.qalsdk.service.QALServiceReceiver" 
+             android:enabled="true" 
+             android:exported="false" 
+             android:process=":QALSERVICE" >
+             <intent-filter android:priority="0x7fffffff">
+                <action android:name="android.intent.action.BOOT_COMPLETED" />        	
+                <action android:name="android.intent.action.USER_PRESENT" />               
+                <action android:name="android.bluetooth.adapter.action.STATE_CHANGED" />
+                <action android:name="android.intent.action.ACTION_POWER_CONNECTED" />
+                <action android:name="android.intent.action.ACTION_POWER_DISCONNECTED" />
+            </intent-filter>
+        </receiver>
+```
+
+2 . 如果需要离线推送通知，需要在application的onCreate中调用TIMManager中的setOfflinePushListener接口注册离线推送通知回调（具体参数可以参考相应的javadoc）。如下：
+   注意：onCreate() 这里只是初始化离线处理必须的逻辑。app正常启动初始化逻辑建议放在activity中。
+   
+```java
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyApplication", "app oncreate");
+        if(MsfSdkUtils.isMainProcess(this)) {
+            Log.d("MyApplication", "main process");
+            TIMManager.getInstance().setOfflinePushListener(new TIMOfflinePushListener() {
+                @Override
+                public void handleNotification(TIMOfflinePushNotification notification) {
+                    Log.e("MyApplication", "recv offline push");
+                    notification.doNotify(getApplicationContext(), R.drawable.ic_launcher);
+                }
+            });
+        }
+    }
+}
+```
+
+
+[参考文档](http://www.qcloud.com/doc/product/269/%E6%A6%82%E8%BF%B0%EF%BC%88Android%20SDK%EF%BC%89)
+
+#### Jar 包说明
+
+|----|----|
+| beacon_android_v1.9.9.jar | 灯塔上报jar包 |
+| bugly_1.2.8_imsdk_release.jar | crash上报jar包 |
+| imsdk.jar | SDK的jar包 |
+| mobilepb.jar | protobuffer处理相关jar包 |
+| qalsdk.jar | SDK网络层jar包 |
+| tls_sdk.jar | 帐号系统jar包 |
+| wup-1.0.0-SNAPSHOT.jar | 无线统一协议jar包 |
+
+#### 更新日志
+
+See [Change Log](CHANGELOG.md)
--- demo/src/main/java/com/example/mydemo/activity/SplashActivity.java
+++ demo/src/main/java/com/example/mydemo/activity/SplashActivity.java
@@ -1,160 +1,157 @@
 package com.example.mydemo.activity;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import tencent.tls.platform.TLSErrInfo;
-import tencent.tls.platform.TLSRefreshUserSigListener;
-import tencent.tls.platform.TLSUserInfo;
-
-import com.example.mydemo.MyApplication;
-import com.example.mydemo.R;
-import com.example.mydemo.TLSHelper;
-import com.example.mydemo.c2c.HttpProcessor;
-import com.example.mydemo.c2c.UserInfoManager;
-import com.example.mydemo.c2c.UserInfoManagerNew;
-import com.example.mydemo.utils.Constant;
-import com.tencent.TIMCallBack;
-import com.tencent.TIMGroupBaseInfo;
-import com.tencent.TIMGroupManager;
-import com.tencent.TIMManager;
-import com.tencent.TIMUser;
-import com.tencent.TIMValueCallBack;
-//import com.tencent.tls.activity.HostLoginActivity;
-import com.tencent.tls.callback.LoginCallBack;
-import com.tencent.tls.model.ErrorCode;
-import com.tencent.tls.model.LoginSession;
-import com.tencent.tls.service.Constants;
-import com.tencent.tls.service.TLSService;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.example.mydemo.MyApplication;
+import com.example.mydemo.R;
+import com.example.mydemo.SDKHelper;
+import com.example.mydemo.TLSHelper;
+import com.example.mydemo.utils.Foreground;
+import com.tencent.tls.callback.LoginCallBack;
+import com.tencent.tls.model.ErrorCode;
+import com.tencent.tls.model.LoginSession;
+import com.tencent.tls.service.TLSService;
+
+import tencent.tls.platform.TLSErrInfo;
+import tencent.tls.platform.TLSUserInfo;
+
+//import com.tencent.tls.activity.HostLoginActivity;
+
 public class SplashActivity extends Activity {
-	private Handler mHandler;
-	private  StringBuilder mStrRetMsg;	
-	private static final String TAG = SplashActivity.class.getSimpleName();
-	public final static int GO_TO_LOGINNEW = 1;
-	public void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.activity_splash);
-		super.onCreate(savedInstanceState);
-		
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		initView();
-	}
+    private Handler mHandler;
+    private StringBuilder mStrRetMsg;
+    private static final String TAG = SplashActivity.class.getSimpleName();
+    public final static int GO_TO_LOGINNEW = 1;
+    private SDKHelper helper = new SDKHelper();
+    private TLSHelper tlsHelper = new TLSHelper();
 
-	public void initView() {
-			mHandler = new Handler(Looper.getMainLooper());
-			mHandler.postDelayed(new Runnable() {
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_splash);
+        super.onCreate(savedInstanceState);
 
-				public void run() {
-					// TODO Auto-generated method stub			
-						if(MyApplication.getInstance().bHostRelaytionShip){
-							Log.d(TAG,"init:" + TLSHelper.userID + ":"+ TLSService.getInstance().needLogin(TLSHelper.userID));
-							if(TLSHelper.userID!= null && (!TLSService.getInstance().needLogin(TLSHelper.userID)) ){								
-								goRefreshTicket();
-							}else{
-								goLoginNewActivity();
-							}
-						}else{
-							goLoginActivity();
-						}
-					}				
-			}, 1000);
-		
-	}
-	
-	public void goLoginNewActivity() {
-		Log.d(TAG,"start activity");
+        onInitSDK();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        initView();
+    }
+
+
+    private void onInitSDK() {
+        helper.init(this.getApplicationContext());
+        tlsHelper.init(this.getApplicationContext());
+        Foreground.init(this.getApplication());
+    }
+
+    public void initView() {
+        mHandler = new Handler(Looper.getMainLooper());
+        mHandler.postDelayed(new Runnable() {
+
+            public void run() {
+                // TODO Auto-generated method stub
+                if (MyApplication.getInstance().bHostRelaytionShip) {
+                    Log.d(TAG, "init:" + TLSHelper.userID + ":" + TLSService.getInstance().needLogin(TLSHelper.userID));
+                    if (TLSHelper.userID != null && (!TLSService.getInstance().needLogin(TLSHelper.userID))) {
+                        goRefreshTicket();
+                    } else {
+                        goLoginNewActivity();
+                    }
+                } else {
+                    goLoginActivity();
+                }
+            }
+        }, 1000);
+
+    }
+
+    public void goLoginNewActivity() {
+        Log.d(TAG, "start activity");
 //	    Intent intent = new Intent(SplashActivity.this, HostLoginActivity.class);
 //	    intent.putExtra(Constants.EXTRA_THIRDAPP_PACKAGE_NAME_SUCC, "com.example.mydemo");
 //	    intent.putExtra(Constants.EXTRA_THIRDAPP_CLASS_NAME_SUCC, "com.example.mydemo.activity.MainActivity");
 
-		TLSService.getInstance().showPhoneSmsLogin(SplashActivity.this, new LoginCallBack(){
+        TLSService.getInstance().showPhoneSmsLogin(SplashActivity.this, new LoginCallBack() {
 
-			@Override
-			public void onSuccess(LoginSession session) {
-				// TODO Auto-generated method stub
-				Log.d(TAG,"tls login succ");
-				Intent intent;
-				intent = new Intent(SplashActivity.this, MainActivity.class);
-				intent.putExtra("session",session);
-				startActivity(intent);
-				finish();
-			}
+            @Override
+            public void onSuccess(LoginSession session) {
+                // TODO Auto-generated method stub
+                Log.d(TAG, "tls login succ");
+                Intent intent;
+                intent = new Intent(SplashActivity.this, MainActivity.class);
+                intent.putExtra("session", session);
+                startActivity(intent);
+                finish();
+            }
 
-			@Override
-			public void onFailure(ErrorCode errCode,
-					String msg) {
-				Log.e(TAG,"tls login Error:" + errCode + ":" + msg);
-				Toast.makeText(getBaseContext(), "登陆失败:"+errCode +"|"+msg, Toast.LENGTH_SHORT).show();
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
-	    //startActivity(intent);
-        
-	}	
-	
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		Log.d(TAG,"acitivy result:" + requestCode);
-	} 
+            @Override
+            public void onFailure(ErrorCode errCode,
+                                  String msg) {
+                Log.e(TAG, "tls login Error:" + errCode + ":" + msg);
+                Toast.makeText(getBaseContext(), "登陆失败:" + errCode + "|" + msg, Toast.LENGTH_SHORT).show();
+                // TODO Auto-generated method stub
 
-	
-	public void goRefreshTicket(){
+            }
+
+        });
+        //startActivity(intent);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "acitivy result:" + requestCode);
+    }
+
+
+    public void goRefreshTicket() {
         // 刷新票据必须实现RefreshUserSigListener接口
-		TLSService.getInstance().refreshUserSig(TLSHelper.userID,new TLSService.RefreshUserSigListener() {
+        TLSService.getInstance().refreshUserSig(TLSHelper.userID, new TLSService.RefreshUserSigListener() {
             @Override
             public void onUserSigNotExist() {
-            	Log.e(TAG,"onUserSigNotExist");
-            	goLoginNewActivity();
+                Log.e(TAG, "onUserSigNotExist");
+                goLoginNewActivity();
             }
 
             @Override
             public void OnRefreshUserSigSuccess(TLSUserInfo tlsUserInfo) {
-            //	LoginToIMServer((Intent)null);
-        		Intent intent = new Intent();
-        		intent.setClass(SplashActivity.this, MainActivity.class);
-        		
-        		startActivity(intent);    
-        		finish();
+                //	LoginToIMServer((Intent)null);
+                Intent intent = new Intent();
+                intent.setClass(SplashActivity.this, MainActivity.class);
+
+                startActivity(intent);
+                finish();
             }
 
             @Override
             public void OnRefreshUserSigFail(TLSErrInfo tlsErrInfo) { // 刷票失败
-				Log.e(TAG,"刷票失败:" + tlsErrInfo.ErrCode + ":" + tlsErrInfo.Msg);
-				goLoginNewActivity();
+                Log.e(TAG, "刷票失败:" + tlsErrInfo.ErrCode + ":" + tlsErrInfo.Msg);
+                goLoginNewActivity();
             }
 
             @Override
             public void OnRefreshUserSigTimeout(TLSErrInfo tlsErrInfo) { // 刷票超时
-				Log.e(TAG,"刷票超时:" + tlsErrInfo.ErrCode + ":" + tlsErrInfo.Msg);
-				goLoginNewActivity();
+                Log.e(TAG, "刷票超时:" + tlsErrInfo.ErrCode + ":" + tlsErrInfo.Msg);
+                goLoginNewActivity();
             }
         });
-	}
-	
-	public void goLoginActivity() {
-		Intent intent = new Intent();
-		intent.setClass(this, LoginActivity.class);
-		startActivity(intent);
-	//	finish();
-	}	
+    }
 
-	  
+    public void goLoginActivity() {
+        Intent intent = new Intent();
+        intent.setClass(this, LoginActivity.class);
+        startActivity(intent);
+        //	finish();
+    }
+
+
 //	 private void LoginToIMServer(Intent data){
 //		String userID ;
 //		String userSig;
